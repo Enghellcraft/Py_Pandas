5eca7cca3f876d76c18217f25f52a2781138802d
Enghell <dmmartin@estudiantes.unsam.edu.ar>
2022-05-15 17:43:11 -0300

    up

--- Practica4.py
+++ Practica4.py
@@ -1,11 +1,19 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
 
 def csv_to_dataframe(path_archivo):
-    data = pd.ExcelFile(path_archivo)
-    print(data.sheet_names)
-    data_frame = data.parse("Autos")
+    data = pd.read_excel(path_archivo, index_col=0)
+    print(data)
+    data_frame = data.parse("autos")
     return data_frame
 
-df = csv_to_dataframe("E:\Mate3\Scrips_Python\Python_VSC\Py_Pandas\14.autos.xlsx")
+df = csv_to_dataframe("autos.xlsx")
 print(df)
+
+df.groupby('TIPO')['STOCK'].sum().plot(kind='barh',
+legend='Reverse')
+plt.xlabel('Stock')
+plt.title('Concesionaria',
+weight='bold',
+size=10)
+plt.show()
